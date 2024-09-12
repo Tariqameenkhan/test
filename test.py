78a048186acff228c7c540a1944919d277a74e60
Tariqameenkhan <tariqameen097@gmail.com>
2024-09-12 08:10:27 -0700

    Add files via upload

---
+++ test.py
@@ -0,0 +1,199 @@
+# name = "ali"
+# print(type(name))
+# name = "zain"
+# # print(f"how are you {name} ?")
+# name = "ali is Good boy"
+# print(name.upper())
+# print(name.lower())
+# print(name.title())
+# print(name.capitalize())
+
+# name = "zain"
+
+# print(f"{name} said, How are you. ")
+
+# name = "  \tAli said\n  "
+# print(name)
+# print(name.rstrip())
+# print(name.strip())
+
+# ver = "ali"
+# print(ver.upper())
+# print(4+4)
+# print(12-4)
+# print(4*2)
+# print(32/4)
+ 
+
+
+# import this
+
+# zxc = 5
+# print(zxc)
+ 
+# list = ["ali","zain","khan"]
+# print(list[2])
+# print(list[1])
+# print(list[0])
+
+# list = ["ali","zain","khan"]
+
+# print(f"{list[2]} how are you ?")
+# print(f"{list[1]} how are you ?")
+# print(f"{list[0]} how are you ?")
+# list[2] = "saad"
+
+# print(f"{list[2]} how are you ?")
+# print(f"{list[1]} how are you ?")
+# print(f"{list[0]} how are you ?")
+
+
+# list = ["ali","zain","khan"]
+# print(f"{list[2]} how are you ?")
+# print(f"{list[1]} how are you ?")
+# print(f"{list[0]} how are you ?")
+
+# list.insert( 3,"saad")
+# list.insert( 4,"sajid")
+# print(f"{list[4]} how are you ?")
+# print(f"{list[3]} how are you ?")
+# list.pop(2)
+# print(list)
+
+# list = ["zain","khan","ali"]
+# print(list.sort())
+# print(list)
+
+# list = ["ali","zain","khan"]
+# list.reverse()
+# print(list)
+
+# list = ["zain","khan","ali"]
+# # print(f"print list {len(list)}")
+# print(len(list))
+# ver = "ali","saadkan"
+# list = ["zain","khan","ali"]
+# list.append(ver)
+# print(list)
+
+# chapter 3
+
+# pizza = ["chicken",123,987]
+# for x in pizza:
+#     print(x)
+
+# for i in range(1,21):
+#     print(i)
+
+# i = list(range(1,101))
+# print(min(i))
+# print(max(i))
+# print(sum(i))
+
+
+# for i in range(1,21):
+#     if i%2 != 0:
+#         print(i)
+
+
+# cubes = [value**3 for value in range(1,11)]
+
+
+# for i in cubes:
+#     print(i)
+
+# tuple = ("chicken","roast", "muton","paneer","roti")
+# for i in tuple:
+#     print(i)
+# tuple[4] = "saag"
+# print(tuple)
+
+# a  = ["ali", "zain", "khan", ]
+# b  = [10,20,30]
+# c  = a + b
+# print(c[4])
+
+# ver = ["ali", "zain", "khan", ] , [10,20,30]
+
+# print(ver[1][0])
+# print(ver[1][1])
+# print(ver[1][2])
+# #
+# print(ver[0][0])
+# print(ver[0][1])
+
+# print(ver[0][2])
+
+
+
+# ver = ["ali", "zain", "khan", ] , [10,20,30] , ["a1", "b2","c3"]
+
+# print(ver[2][0])
+# print(ver[2][1])
+# print(ver[2][2])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
